b30b42c45fdffa8c268116b56e5dd7e076ced455
TheKitoInc <11139894+TheKitoInc@users.noreply.github.com>
2026-06-29 19:01:44 -0300

    Update disableNewsFeeds.bat

--- Scripts/disableNewsFeeds.bat
+++ Scripts/disableNewsFeeds.bat
@@ -7,16 +7,32 @@ cls
 
 call "%~dp0Helpers\showTitle.bat" "Disable News and Interests"
 
-taskkill /F /IM explorer.exe
+:: 0. IMPORTANT: stop shell first, not just explorer
+taskkill /f /im explorer.exe
+taskkill /f /im ShellExperienceHost.exe >nul 2>&1
+taskkill /f /im StartMenuExperienceHost.exe >nul 2>&1
 
-reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Feeds" /v "ShellFeedsTaskbarPreviousViewMode" /t REG_DWORD /d "2" /f
+:: 1. Set POLICY FIRST (most important)
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Feeds" ^
+ /v EnableFeeds ^
+ /t REG_DWORD ^
+ /d 0 ^
+ /f
 
-:: Windows 10
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Feeds" /v ShellFeedsTaskbarViewMode /t REG_DWORD /d 2 /f >nul
-reg add "HKCU\Software\Policies\Microsoft\Windows\Windows Feeds" /v EnableFeeds /t REG_DWORD /d 0 /f
+:: 2. User-level fallback (taskbar UI state)
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Feeds" ^
+ /v ShellFeedsTaskbarViewMode ^
+ /t REG_DWORD ^
+ /d 2 ^
+ /f
 
-:: Windows 11
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarDa /t REG_DWORD /d 0 /f >nul
+:: 3. ALSO disable search highlights (keeps feed behavior alive visually)
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\SearchSettings" ^
+ /v IsDynamicSearchBoxEnabled ^
+ /t REG_DWORD ^
+ /d 0 ^
+ /f
+
 
 call "%~dp0Helpers\restartExplorer.bat"
 
