f17fbfbe2104015c4cf7e6eebde5506d37898648
TheKitoInc <11139894+TheKitoInc@users.noreply.github.com>
2026-06-29 18:40:42 -0300

    Update disableNewsFeeds.bat

--- Scripts/disableNewsFeeds.bat
+++ Scripts/disableNewsFeeds.bat
@@ -11,6 +11,7 @@ reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Feeds" /v "
 
 :: Windows 10
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Feeds" /v ShellFeedsTaskbarViewMode /t REG_DWORD /d 2 /f >nul
+reg add "HKCU\Software\Policies\Microsoft\Windows\Windows Feeds" /v EnableFeeds /t REG_DWORD /d 0 /f
 
 :: Windows 11
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v TaskbarDa /t REG_DWORD /d 0 /f >nul
