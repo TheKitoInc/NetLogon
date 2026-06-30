0013746de27839ecc85809a575c04472469b75d1
TheKitoInc <11139894+TheKitoInc@users.noreply.github.com>
2026-06-29 23:01:41 -0300

    Update clean.bat

--- Scripts/clean.bat
+++ Scripts/clean.bat
@@ -31,36 +31,36 @@ if %errorlevel% == 0 (
 echo Running user cleanup tasks...
 echo.
 
-if exist "%BASEDIR%cleanTemp.bat" (
-    call "%BASEDIR%cleanTemp.bat"
+if exist "%BASEDIR%Clean\cleanTemp.bat" (
+    call "%BASEDIR%Clean\cleanTemp.bat"
 )
 
-if exist "%BASEDIR%cleanChrome.bat" (
-    call "%BASEDIR%cleanChrome.bat"
+if exist "%BASEDIR%Clean\cleanChrome.bat" (
+    call "%BASEDIR%Clean\cleanChrome.bat"
 )
 
-if exist "%BASEDIR%cleanChromium.bat" (
-    call "%BASEDIR%cleanChromium.bat"
+if exist "%BASEDIR%Clean\cleanChromium.bat" (
+    call "%BASEDIR%Clean\cleanChromium.bat"
 )
 
-if exist "%BASEDIR%cleanEdge.bat" (
-    call "%BASEDIR%cleanEdge.bat"
+if exist "%BASEDIR%Clean\cleanEdge.bat" (
+    call "%BASEDIR%Clean\cleanEdge.bat"
 )
 
-if exist "%BASEDIR%cleanFirefox.bat" (
-    call "%BASEDIR%cleanFirefox.bat"
+if exist "%BASEDIR%Clean\cleanFirefox.bat" (
+    call "%BASEDIR%Clean\cleanFirefox.bat"
 )
 
-if exist "%BASEDIR%cleanBrave.bat" (
-    call "%BASEDIR%cleanBrave.bat"
+if exist "%BASEDIR%Clean\cleanBrave.bat" (
+    call "%BASEDIR%Clean\cleanBrave.bat"
 )
 
-if exist "%BASEDIR%cleanRecycleBin.bat" (
-    call "%BASEDIR%cleanRecycleBin.bat"
+if exist "%BASEDIR%Clean\cleanRecycleBin.bat" (
+    call "%BASEDIR%Clean\cleanRecycleBin.bat"
 )
 
-if exist "%BASEDIR%cleanPrinters.bat" (
-    call "%BASEDIR%cleanPrinters.bat"
+if exist "%BASEDIR%Clean\cleanPrinters.bat" (
+    call "%BASEDIR%Clean\cleanPrinters.bat"
 )
 
 echo.
@@ -76,28 +76,28 @@ if "%IS_ADMIN%"=="1" (
     echo Running administrator cleanup tasks...
     echo.
 
-    if exist "%BASEDIR%cleanWindowsUpdate.bat" (
-        call "%BASEDIR%cleanWindowsUpdate.bat"
+    if exist "%BASEDIR%Clean\cleanWindowsUpdate.bat" (
+        call "%BASEDIR%Clean\cleanWindowsUpdate.bat"
     )
 
-    if exist "%BASEDIR%cleanPrefetch.bat" (
-        call "%BASEDIR%cleanPrefetch.bat"
+    if exist "%BASEDIR%Clean\cleanPrefetch.bat" (
+        call "%BASEDIR%Clean\cleanPrefetch.bat"
     )
 
-    if exist "%BASEDIR%cleanUVNC.bat" (
-        call "%BASEDIR%cleanUVNC.bat"
+    if exist "%BASEDIR%Clean\cleanUVNC.bat" (
+        call "%BASEDIR%Clean\cleanUVNC.bat"
     )
 
-    if exist "%BASEDIR%cleanDISM.bat" (
-        call "%BASEDIR%cleanDISM.bat"
+    if exist "%BASEDIR%Clean\cleanDISM.bat" (
+        call "%BASEDIR%Clean\cleanDISM.bat"
     )
 
-    if exist "%BASEDIR%cleanDisk.bat" (
-        call "%BASEDIR%cleanDisk.bat"
+    if exist "%BASEDIR%Clean\cleanDisk.bat" (
+        call "%BASEDIR%Clean\cleanDisk.bat"
     )
 
-    if exist "%BASEDIR%cleanErrorReports.bat" (
-        call "%BASEDIR%cleanErrorReports.bat"
+    if exist "%BASEDIR%Clean\cleanErrorReports.bat" (
+        call "%BASEDIR%Clean\cleanErrorReports.bat"
     )
 
     echo.
